5989ccb34c022004d59528ba7c208bc134d2ffb5
rafaelsanchezsouza <rafaelsanchezsouza@gmail.com>
2020-03-26 22:03:07 -0300

    Header Test ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Header from "./Header";
 
 function App() {
+  const [counter,setCounter] = useState(0);
+
+  //Array [valor,funcaoDeAtualizacao]
+
+  function increment() {
+    setCounter(counter + 1);
+    console.log(counter);
+  }
+
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Header>Contador: {counter}</Header>
+      <button onClick={increment}>Incrementar</button>
+    </div>
   );
 }
 
