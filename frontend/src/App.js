0004aa7de19db45b019f0a41e6ff69ff9e057ec6
rafaelsanchezsouza <rafaelsanchezsouza@gmail.com>
2020-03-27 09:19:13 -0300

    Logon and Register working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import Header from "./Header";
+import "./global.css"
+
+import Routes from "./routes";
 
 function App() {
-  const [counter,setCounter] = useState(0);
-
-  //Array [valor,funcaoDeAtualizacao]
-
-  function increment() {
-    setCounter(counter + 1);
-    console.log(counter);
-  }
-
   return (
-    <div>
-      <Header>Contador: {counter}</Header>
-      <button onClick={increment}>Incrementar</button>
-    </div>
+    < Routes />
   );
 }
 
