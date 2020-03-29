4147bea9b9c4ee18611ff750615131f7998ae72d
rafaelsanchezsouza <rafaelsanchezsouza@gmail.com>
2020-03-29 09:54:23 -0300

    whatsapp and sendmail NOT working

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,24 +1,46 @@
 import React from "react";
 import { Feather } from "@expo/vector-icons";
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import {useNavigation } from "@react-navigation/native";
+import { View, Text, Image, TouchableOpacity, Linking } from "react-native";
+import * as MailComposer from "expo-mail-composer";
 
 import logoImg from "../../assets/logo.png";   
 
 import styles from "./styles"
 
 export default function Detail() {
+    const navigation = useNavigation();
+    const message = "Olá APAD, estrou entrando em contato pois gostaria de ajudar no caso 'Cadelinha Atropelada' com o valor de 120,00";
+
+    function navigateBack() {
+        navigation.goBack()
+    }
+
+    function sendMail() {
+        MailComposer.composeAsync({
+            subject: "Herói do caso: Cadelinha atropelada",
+            recipents: ["diego@rocketseat.com.br"],
+            body: message,
+        })
+    }
+
+    function sendWhatsapp() {
+        Linking.openURL(`whatsapp://send?phone=5583981249304&text=${message}`);
+    }
+
+
     return ( 
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image source={logoImg}/>
 
-                <TouchableOpacity onPress={() => {}}>
+                <TouchableOpacity onPress={navigateBack}>
                     <Feather name="arrow-left" size={28} color="#E82041"/>
                 </TouchableOpacity>
             </View>
 
             <View style={styles.incident}>
-                <Text style={styles.incidentProperty}>ONG:</Text>
+                <Text style={[styles.incidentProperty, {marginTop: 0}]}>ONG:</Text>
                 <Text style={styles.incidentValue}>APAD</Text>
 
                 <Text style={styles.incidentProperty}>CASO:</Text>
@@ -35,11 +57,11 @@ export default function Detail() {
             <Text style={styles.heroDescription}>Entre em contato:</Text>
 
             <View style={styles.actions}>
-                <TouchableOpacity style={styles.action } onPress={()=>{}}>
+                <TouchableOpacity style={styles.action } onPress={sendWhatsapp}>
                     <Text style={styles.actionText}>Whatsapp</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity style={styles.action } onPress={()=>{}}>
+                <TouchableOpacity style={styles.action } onPress={sendMail}>
                     <Text style={styles.actionText}>E-mail</Text>
                 </TouchableOpacity>
 
